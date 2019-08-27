3d2504d546009169b40d26ee24802290981f311e
nicoa47 <nicoadelh47@gmail.com>
2019-08-27 10:38:24 +0200

    d

--- game.js
+++ game.js
@@ -11,7 +11,8 @@ var whole_len = Math.min(canvas.width, canvas.height);
 var absolute;
 var alpha;   
 var beta;    
-var gamma;   
+var gamma;
+var in_func = false;   
 
 function new_level(set_n_rings) {
     n_rings = set_n_rings;
@@ -764,6 +765,9 @@ function render() {
     maze.render();
     player.render();
     debug_draw_text(alpha);
+    if (in_func) {
+        debug_draw_text("HERE!!!");
+    }
 }
 
 function keydown(e) {
@@ -803,6 +807,7 @@ function device_rotation(e) {
     alpha    = e.alpha;
     beta     = e.beta;
     gamma    = e.gamma;
+    in_func = true;
 }
 
 // start the updating loop
