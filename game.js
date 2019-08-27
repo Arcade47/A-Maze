7c66644b6814df70a2abb244d59d542c9531b839
nicoa47 <nicoadelh47@gmail.com>
2019-08-27 11:03:03 +0200

    dd

--- game.js
+++ game.js
@@ -759,7 +759,7 @@ function update() {
 
 function render() {
     // refresh
-    refresh_canvas("white");
+    refresh_canvas("lightblue");
     // draw objects
     maze.render();
     player.render();
